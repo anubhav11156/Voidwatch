46973bde5e080d019ba565f6866b1f1f66fd7ff0
Anubhav Kumar <anubhav11697@gmail.com>
2023-04-14 18:41:42 +0530

    more bug fix

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,7 +10,7 @@ import (
 )
 
 
-// const port = 8080
+const port = 8080
 const atlasConnectionUri = "mongodb+srv://anubhav11697:myMongo123@myfirstcluster.hfdwigv.mongodb.net/?retryWrites=true&w=majority"
 
 type application struct {
@@ -34,7 +34,7 @@ func main(){
 
 	// starts a web server
 	// err := http.ListenAndServe(fmt.Sprintf("https://subwatch-backend.onrender.com:%d",port), app.routes())
-	err := http.ListenAndServe(fmt.Sprintf("subwatch-backend.onrender.com"), app.routes())
+	err := http.ListenAndServe(fmt.Sprintf("subwatch-backend.onrender.com:",port), app.routes())
 
 	if err != nil {
 		log.Fatal(err)
