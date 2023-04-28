8ad0a2015f6e137e50bf9c5274c57642c388c290
Anubhav Kumar <anubhav11697@gmail.com>
2023-04-28 11:42:57 +0530

    late commit

---
+++ src/components/Movies.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+function Movies() {
+
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+
+        const headers = new Headers();
+
+        headers.append("Content-Type", "application/json");
+
+        const requestOptions = {
+            method: "GET",
+            headers: headers,
+        }
+
+        fetch(`http://localhost:8080/getAllMovies`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log('data : ', data)
+                setMovies(data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    },[])
+
+    return (
+        <Container>
+            <p>All the movies!</p>
+            <hr />
+            <table className='table table-striped table-bordered table-hover'>
+                <thead>
+                    <tr>
+                        <th>Movie</th>
+                        <th>Release Date</th>
+                        <th>Rating</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {movies.map((m) => (
+                        <tr key={m.id}>
+                            <td>
+                                <Link to={`/movies/${m.id}`}>{m.title}</Link>
+                            </td>
+                            <td>
+                                {m.release_date}
+                            </td>
+                            <td>
+                                {m.mpaa_rating}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </Container>
+    )
+}
+
+export default Movies
+
+const Container = styled.div`
+    font-weight: 200;
+    width: 99%;
+    p {
+        font-size: 32px;
+        font-weight: 600;
+        margin:0;
+    }
+
+
+
+    thead {
+        font-size: 16px;
+        font-weight: 300;
+        font-family: poppins;
+    }
+
+    tbody {
+        font-weight: 300;
+        cursor: pointer;
+    }
+`
---
+++ src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import ErrorPage from './components/ErrorPage';
+import Home from './components/Home';
+import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from 'react-router-dom'
+import Movies from './components/Movies';
+import Genres from './components/Genres';
+import Movie from './components/Movie';
+import ManageCatlouge from './components/ManageCatlouge';
+import GraphQl from './components/GraphQl';
+import Login from './components/Login';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {index: true, element: <Home />},
+      {
+        path:'/movies',
+        element: <Movies />
+      },
+      {
+        path:'/movies/:id',
+        element: <Movie />
+      },
+      {
+        path:'/genres',
+        element: <Genres />
+      },
+      {
+        path:'/admin/movie/0',
+        element: <Movie />
+      },
+      {
+        path:'/manage-catalouge',
+        element: <ManageCatlouge />
+      },
+      {
+        path:'/graphql',
+        element: <GraphQl />
+      },
+      {
+        path:'/login',
+        element: <Login />
+      },
+    ]
+  }
+]) 
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
