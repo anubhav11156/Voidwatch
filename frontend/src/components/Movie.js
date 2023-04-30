eb142ff8f9314f95ed6398bd10f2050e63aa1369
Anubhav Kumar <anubhav11697@gmail.com>
2023-04-30 13:37:04 +0530

    add: genre to movie component

--- frontend/src/components/Movie.js
+++ frontend/src/components/Movie.js
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import styled from 'styled-components' 
+import styled from 'styled-components'
 
 
 function Movie() {
 
-    let {id} = useParams()
+    let { id } = useParams()
     const [movie, setMovie] = useState({})
 
     useEffect(() => {
@@ -25,20 +25,39 @@ function Movie() {
             .catch((error) => {
                 console.log(error)
             })
-    },[id])
+    }, [id])
 
-  return (
-    <Container>
-        <p className='movieName'>Movie: {movie.title}</p>
-        <small><pre><em>Release Date: {movie.release_date}   Runtime: {movie.runtime}   Rated: {movie.mpaa_rating}</em></pre></small>
-        <hr />
-        <div>
-            <img src={`https://image.tmdb.org/t/p/w200${movie.image}`} alt='poster'/>
-        </div>
-        <hr />
-        <p className='description'>{movie.description}</p>
-    </Container>
-  )
+    if (movie.genres) {
+        movie.genres = Object.values(movie.genres)
+    } else {
+        movie.genres = [] // empty array if it doesn't exists
+    }
+
+    return (
+        <Container>
+            <p className='movieName'>Movie: {movie.title}</p>
+            <small><pre><em>Release Date: {movie.release_date}   Runtime: {movie.runtime}   Rated: {movie.mpaa_rating}</em></pre></small>
+            <small>{movie.genres.map((g) => {
+                return (
+                    <span key={g._id} className='badge bg-secondary me-2' style={{
+                        color: 'white',
+                        marginRight: '10px',
+                        fontSize: '11px',
+                        fontWeight: '600'
+                    }}>{g.genre}</span>
+                )
+            })}</small>
+            <hr />
+            {movie.image !== "" &&
+                <div>
+                    <img src={`https://image.tmdb.org/t/p/w200${movie.image}`} alt='poster' />
+                </div>
+            }
+
+            <hr />
+            <p className='description'>{movie.description}</p>
+        </Container>
+    )
 }
 
 export default Movie
